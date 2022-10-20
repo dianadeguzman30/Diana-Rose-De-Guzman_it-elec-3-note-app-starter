014a57aebfb6a8a2b46ccab812807aefdfa6fee8
Diana <dianadeguzman329@gmail.com>
2022-10-20 18:50:40 +0800

    init

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+const add = require ('./add')
 const read = require ('./read')
 const write = require ('./write')
 
 
-const data = process.argv[2]
+const data = process.argv 
 
-write(data)
+var note = {}
+
+if(data[2] == 'add') {
+    // Build Object
+    note = { 
+        id: data[3],
+        title: data[4],
+        body: data[5]
+    }
+    // Get old note value
+    var oldNote = read()
+    // Add note to note.txt
+    add(note, oldNote)
+}
