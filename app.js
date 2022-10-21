1d2fa07e3898c07143d0011e106cd1dfe8ed1741
Bine <hubillahubillahubilla@gmail.com>
2022-10-21 14:48:51 +0800

    init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const add = require ('./add')
 const read = require ('./read')
 const write = require ('./write')
 const present = require('./present')
+const update = require('./update')
 
 
 
@@ -20,8 +21,33 @@ if(cmd[2] == 'add') {
     var oldNote = read()
     // Add note to note.txt
     add(note, oldNote)
+    console.log(note, oldNote)
 }
 if(cmd[2] == 'read') {
     present(read())
 }
+const fs = require ('fs')
 
+const del = function (id, oldNote){
+const note = JSON.parse(oldNote)
+
+const newNote = note.filter(function(n,idx) {
+
+        return n.id !== id
+})
+fs.writeFileSync('note.txt', JSON.stringify(newNote))
+}
+
+module.exports = del
+if(cmd[2] == 'update')  {
+    const note = {
+      id: cmd[3],
+      title: cmd[4],
+      body: cmd[5]
+    }
+    const oldNote = read()
+
+    update(note,oldNote)
+
+    console.log(note, oldNote)
+}
